b1a9c2c6043b70e1ad9633b92ed96531be480b41
DimaChekashov <foxdima99@gmail.com>
2022-07-03 17:56:59 +0300

    implemets CRUD posts

---
+++ validations.js
@@ -0,0 +1,20 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+    body("email", "Invalid Email").isEmail(),
+    body("password", "Invalid Password min 5 words").isLength({ min: 5 }),
+];
+
+export const registerValidation = [
+    body("email", "Invalid Email").isEmail(),
+    body("password", "Invalid Password min 5 words").isLength({ min: 5 }),
+    body("fullName", "Invalid Full Name min 3 words").isLength({ min: 3 }),
+    body("avatarUrl", "Invalid Avatar Url").optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body("title", "Enter post title").isLength({ min: 3 }).isString(),
+    body("text", "Enter post text").isLength({ min: 3 }).isString(),
+    body("tags", "Incorrect tags format (Enter array)").optional().isString(),
+    body("imageUrl", "Incorrect image Url").optional().isString(),
+];
--- validations/auth.js
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator";
-
-export const registerValidation = [
-    body("email", "Invalid Email").isEmail(),
-    body("password", "Invalid Password min 5 words").isLength({ min: 5 }),
-    body("fullName", "Invalid Full Name min 3 words").isLength({ min: 3 }),
-    body("avatarUrl", "Invalid Avatar Url").optional().isURL(),
-];
