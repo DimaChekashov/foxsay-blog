8524470422fdc39f9d05db70df50a58b687fcbf2
DimaChekashov <foxdima99@gmail.com>
2022-07-03 16:02:11 +0300

    implements login

--- index.js
+++ index.js
@@ -10,14 +10,61 @@ import UserModel from "./models/User.js";
 
 mongoose
     .connect(
-        "mongodb+srv://admin:123@cluster0.r2yk4p0.mongodb.net/blog?retryWrites=true&w=majority"
+        // "mongodb+srv://admin:123@cluster0.r2yk4p0.mongodb.net/blog?retryWrites=true&w=majority"
+        "mongodb://admin:123@ac-gsmzegu-shard-00-00.r2yk4p0.mongodb.net:27017,ac-gsmzegu-shard-00-01.r2yk4p0.mongodb.net:27017,ac-gsmzegu-shard-00-02.r2yk4p0.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-c0z872-shard-0&authSource=admin&retryWrites=true&w=majority"
     )
     .then(() => console.log("DB ok"))
-    .catch(() => console.log("DB error", err));
+    .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/auth/login", async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found!",
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(
+            req.body.password,
+            user._doc.passwordHash
+        );
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: "Incorrect login or password!",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            "secret123",
+            {
+                expiresIn: "30d",
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: "Can't authorization!",
+        });
+    }
+});
 
 app.post("/auth/register", registerValidation, async (req, res) => {
     try {
